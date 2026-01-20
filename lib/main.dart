9d0590b4cc6bb3d34ac707753659d3b652359d31
Bakanisara <suree7913@.com>
2026-01-20 15:57:57 +0700

    finish column

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_week14/view/Home.dart';
+import 'package:flutter_week14/view/column_page.dart';
 import 'package:flutter_week14/view/row_page.dart';
 
 void main(){
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter App",
-      home: RowPage(
+      home: ColumnPage(
       ),
     );
   }
