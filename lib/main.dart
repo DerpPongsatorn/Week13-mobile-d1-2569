5f1b6ef42f4f578735638bceaae8e1b2634d4037
Bakanisara <suree7913@.com>
2026-01-20 14:26:11 +0700

    first init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_week14/view/Home.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter App",
+      home: HomeScreen(
+
+      ),
+    );
+  }
+}
---
+++ lib/view/Home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        title: Text("Flutter Development"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Mr.Mark Suckerberg"),
+              accountEmail: Text("Mark@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Icon(Icons.android),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                debugPrint("TEST ok");
+              },
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text("MyApp"),
+      ),
+    );
+  }
+}
